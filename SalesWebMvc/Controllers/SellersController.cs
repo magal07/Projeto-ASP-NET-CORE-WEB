34ac6124df85e7d6342d00f7c02993bf3fb8f7dc
Marcelo Magalhães <celoxd01@gmail.com>
2025-02-05 18:10:31 -0300

    SellerController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller // este controlador recebeu a chamada do caminho /sellers
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
