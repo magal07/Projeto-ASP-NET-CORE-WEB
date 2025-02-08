f8c030bd98b5c48ea3e35d5c0bcae2f163c8a00f
Marcelo Magalhães <celoxd01@gmail.com>
2025-02-08 17:03:09 -0300

    The End

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -26,13 +26,10 @@ namespace SalesWebMvc
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
